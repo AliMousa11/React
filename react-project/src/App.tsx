39c2a4211fd14d341842d37068655f7e4144013d
AliMousa11 <alimousa2003@icloud.com>
2024-08-23 23:24:48 +0300

    form + submitHandling + useRef to access input fields

--- react-project/src/App.tsx
+++ react-project/src/App.tsx
@@ -1,24 +1,12 @@
 import "./App.css";
 import "./Message";
 import "./components/ExpandableText";
-import ExpandableText from "./components/ExpandableText";
+import Form from "./components/Form";
 function App() {
   return (
-    <>
-      <ExpandableText length={50}>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et dicta
-        aliquam optio totam sit quas ratione. Assumenda deserunt quasi et
-        commodi natus delectus quibusdam, expedita eligendi quia, nemo dolorum
-        quas ut placeat neque quisquam! Nostrum id nemo numquam eos dolore
-        reprehenderit assumenda suscipit ratione, nobis totam voluptates! Quia
-        nostrum eligendi, nemo nesciunt nisi iure repellendus neque consequatur
-        ad aperiam saepe repudiandae voluptates quisquam veritatis soluta quos
-        quidem veniam molestias laudantium porro enim ex culpa? Officia repellat
-        atque doloribus fuga id iste iure neque dolores velit, porro culpa
-        officiis deserunt debitis quidem veritatis laborum, tenetur enim
-        distinctio cupiditate repudiandae amet aperiam?
-      </ExpandableText>
-    </>
+    <div>
+      <Form />
+    </div>
   );
 }
 
