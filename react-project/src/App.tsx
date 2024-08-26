f7996dc4f8be9b3601f315d6fd346d0c762e8793
AliMousa11 <alimousa2003@icloud.com>
2024-08-26 14:27:25 +0300

    Expense Tracker : implemented expenseList and expenseFilter

--- react-project/src/App.tsx
+++ react-project/src/App.tsx
@@ -1,11 +1,33 @@
+import { useState } from "react";
 import "./App.css";
-import "./Message";
-import "./components/ExpandableText";
-import Form from "./components/Form";
+import ExpenseList from "./ExpenseTracker/components/ExpenseList";
+import ExpenseFilter from "./ExpenseTracker/components/ExpenseFilter";
 function App() {
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "aaa", amount: 10, category: "Utilities" },
+    { id: 2, description: "bbb", amount: 10, category: "Utilities" },
+    { id: 3, description: "ccc", amount: 10, category: "Utilities" },
+    { id: 4, description: "ddd", amount: 10, category: "Utilities" },
+  ]);
+
+  const visibleExpenses = selectedCategory
+    ? expenses.filter((e) => e.category === selectedCategory)
+    : expenses;
+
   return (
     <div>
-      <Form />
+      <div className="mb-3">
+        <ExpenseFilter
+          onSelectCategory={(category) => setSelectedCategory(category)}
+        />
+      </div>
+      <ExpenseList
+        expenses={visibleExpenses}
+        onDelete={(id) =>
+          setExpenses(expenses.filter((expense) => expense.id !== id))
+        }
+      />
     </div>
   );
 }
