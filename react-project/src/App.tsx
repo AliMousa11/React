7291437cc0d4f5f1bf68b957cdafb455fd34095f
AliMousa11 <alimousa2003@icloud.com>
2024-08-27 15:46:10 +0300

    handling errors, cancelling fetch requests

--- react-project/src/App.tsx
+++ react-project/src/App.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import axios from "axios";
+import axios, { CanceledError } from "axios";
 
 interface User {
   id: number;
@@ -9,13 +9,26 @@ interface User {
 }
 function App() {
   const [users, setUsers] = useState<User[]>([]);
+  const controller = new AbortController();
+  const [error, setError] = useState("");
   useEffect(() => {
     axios
-      .get<User[]>("https://jsonplaceholder.typicode.com/users")
-      .then((res) => setUsers(res.data));
-  });
+      .get<User[]>("https://jsonplaceholder.typicode.com/users", {
+        signal: controller.signal,
+      })
+      .then((res) => setUsers(res.data))
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+      });
+
+    console.log("Ali");
+    return () => controller.abort();
+  }, []);
+
   return (
     <div>
+      {error && <p className="text-danger">{error}</p>}
       <ul>
         {users.map((user) => (
           <li className="mb-2" key={user.id}>
