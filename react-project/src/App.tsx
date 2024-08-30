23771fd68360fe35529c4277e0fe11c4bee292c9
AliMousa11 <alimousa2003@icloud.com>
2024-08-30 20:48:54 +0300

    Extracting a Reusable API Client

--- react-project/src/App.tsx
+++ react-project/src/App.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import axios, { CanceledError } from "axios";
-
+import apiClient, { CanceledError } from "./services/api-client";
 interface User {
   id: number;
   name: string;
@@ -17,8 +16,8 @@ function App() {
     const originalUsers = [...users];
     const newUser = { id: 0, name: "Ali", username: "Mousa" };
     setUsers([...users, newUser]);
-    axios
-      .post("https://jsonplaceholder.typicode.com/users/", newUser)
+    apiClient
+      .post("/users/", newUser)
       .then((res) => setUsers([...users, res.data]))
       .catch((err) => {
         setError(err.message);
@@ -30,33 +29,26 @@ function App() {
     const originalUsers = [...users];
     const updatedUser = { ...user, name: user.name + "!" };
     setUsers(users.map((u) => (u.id === user.id ? updatedUser : u)));
-    axios
-      .patch(
-        "https://jsonplaceholder.typicode.com/users/" + user.id,
-        updatedUser
-      )
-      .catch((err) => {
-        setError(err.message);
-        setUsers(originalUsers);
-      });
+    apiClient.patch("/users/" + user.id, updatedUser).catch((err) => {
+      setError(err.message);
+      setUsers(originalUsers);
+    });
   };
 
   const deleteUser = (user: User) => {
     const originalUsers = [...users];
     setUsers(users.filter((u) => u.id !== user.id));
-    axios
-      .delete("https://jsonplaceholder.typicode.com/users/" + user.id)
-      .catch((err) => {
-        setError(err.message);
-        setUsers(originalUsers);
-      });
+    apiClient.delete("/users/" + user.id).catch((err) => {
+      setError(err.message);
+      setUsers(originalUsers);
+    });
   };
 
   useEffect(() => {
     const controller = new AbortController();
     setLoading(true);
-    axios
-      .get<User[]>("https://jsonplaceholder.typicode.com/users", {
+    apiClient
+      .get<User[]>("/users", {
         signal: controller.signal,
       })
       .then((res) => {
